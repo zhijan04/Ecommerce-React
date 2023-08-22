c961b681d75795bbbe4ebb27982429db64e2c454
zhijan <leobenitez0214@gmail.com>
2023-08-22 19:31:35 -0300

    se modifica greeting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='navbar'>
       <NavBar/>
-      <ItemListContainer/>
+      <ItemListContainer greeting={'¡Bienvenidos!'}/>
           </div>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ItemListContainer = ( { greeting } ) => {
     return (
-        <div> <h1>{greeting}</h1></div>
+        <div>
+            <h1 className='greeting'>{greeting}</h1>
+            </div>
     )
 }
 
